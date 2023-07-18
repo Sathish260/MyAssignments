562c1d2c50694e235511944de9fb699f2924536b
Sathish260 <132818664+Sathish260@users.noreply.github.com>
2023-07-18 21:46:40 +0530

    Add files via upload

---
+++ Mobiles.java
@@ -0,0 +1,24 @@
+package julyday1;
+
+public class Mobiles {
+	public static void main(String[]args){
+		String mobileBrandName="Iphone";
+		System.out.println(mobileBrandName);
+		char mobileLogo='a';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece=10;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=14;
+		System.out.println(modelNumber);
+		long mobilelmeiNumber=45324567L;
+		System.out.println( mobilelmeiNumber);
+		float mobilePrice=180000f;
+		System.out.println(mobilePrice);
+		boolean Damaged=false;
+		System.out.println(Damaged);
+		
+		
+	}
+
+
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,21 @@
+package julyday1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels = 2;
+		System.out.println(noOfWheels);
+		short noOfMirrons = 3;
+		System.out.println(noOfMirrons);
+		long chassisNumber = 123476554L;
+		System.out.println(chassisNumber);
+		boolean isPunctured = false;
+		System.out.println(isPunctured);
+		String bikeName = "MT";
+		System.out.println(bikeName);
+		double runningKM = 150.0;
+		System.out.println(runningKM);
+
+	}
+
+
+}
